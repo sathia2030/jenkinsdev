8fa2cb35c912ea4ffab8e4881be8b1dd11775105
AravindGajesh <AravindGajesh@192.168.0.10>
2020-12-04 23:28:00 +0530

    Jenkins Beanstalk Initial Project Setup Push

---
+++ src/main/java/com/demo/beanstalk/JenkinsBeanStalkApplication.java
@@ -0,0 +1,13 @@
+package com.demo.beanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBeanStalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBeanStalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/beanstalk/JenkinsBeanStalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.beanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBeanStalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
